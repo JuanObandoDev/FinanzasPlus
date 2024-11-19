35d957819157edc12375b54e633eecd78ee98d1c
Juan Obando <juanobando.developer@gmail.com>
2024-11-19 18:29:08 -0500

    feat: add sidebar menu and help modal

---
+++ backend/inicio.js
@@ -0,0 +1,30 @@
+function openNav() {
+  document.getElementById("sidebar").style.width = "250px";
+  document.getElementById("sidebar").style.display = "block";
+}
+
+function closeNav() {
+  document.getElementById("sidebar").style.width = "0";
+}
+
+document.getElementById("usuarioIcon").onclick = function () {
+  openNav();
+};
+
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("dudasIcon");
+var span = document.getElementById("closeModal");
+
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
