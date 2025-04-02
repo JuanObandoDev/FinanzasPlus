f7a86563d659d7b250408a2cfbf391be5fa6ba71
Juan Obando <juanobando.developer@gmail.com>
2025-04-01 19:32:14 -0500

    refactor: give better organization of the code

--- backend/registro_de_gastos.js
+++ backend/registro_de_gastos.js
@@ -1,6 +1,7 @@
 const SUPABASE_URL = "https://fghnnxllxilqupwtrezh.supabase.co";
-    const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZnaG5ueGxseGlscXVwd3RyZXpoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDMxNzcyNjEsImV4cCI6MjA1ODc1MzI2MX0.6UiQbo7HZw_Ww1VNFbhRHVeSYz8C-parH1raEAy1_Uk";
-    const SUPABASE = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+const SUPABASE_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZnaG5ueGxseGlscXVwd3RyZXpoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDMxNzcyNjEsImV4cCI6MjA1ODc1MzI2MX0.6UiQbo7HZw_Ww1VNFbhRHVeSYz8C-parH1raEAy1_Uk";
+const SUPABASE = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 window.onload = async function (event) {
   event.preventDefault();
@@ -70,19 +71,20 @@ document
     const id_usuario = localStorage.getItem("userId");
 
     if (monto <= 0) {
-        alert("El monto debe ser un número positivo.");
-        return;
+      alert("El monto debe ser un número positivo.");
+      return;
     }
 
-    const id_categoria_gasto = await SUPABASE.from("categorias_gastos").select(
-      "id_categoria_gasto"
-    ).eq("nombre",categoria).single(); 
+    const id_categoria_gasto = await SUPABASE.from("categorias_gastos")
+      .select("id_categoria_gasto")
+      .eq("nombre", categoria)
+      .single();
 
     const data = {
-        id_usuario: id_usuario,
-        monto: monto,
-        id_categoria_gasto: id_categoria_gasto.data.id_categoria_gasto,
-        descripcion:descripcion,
+      id_usuario: id_usuario,
+      monto: monto,
+      id_categoria_gasto: id_categoria_gasto.data.id_categoria_gasto,
+      descripcion: descripcion,
     };
 
     try {
@@ -98,10 +100,12 @@ document
 
       if (!response.ok) {
         const errorData = await response.json();
-        throw new Error(errorData.message ||"Error al guardar los datos en el servidor");
+        throw new Error(
+          errorData.message || "Error al guardar los datos en el servidor"
+        );
       }
 
-      alert("su gasto ha sido registrada exitosamente");
+      alert("su gasto ha sido registrado exitosamente");
       window.location.href = "../pages/gastos.html";
     } catch (error) {
       alert(error.message);
